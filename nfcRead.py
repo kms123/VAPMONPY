c5bfabb69b47d9e29d09ab15a37e83277dcdd1db
kms123 <sheasby23@me.com>
2015-02-24 20:01:40 -0600

    Started adding functionality for LCD in nfcRead.py.

--- nfcRead.py
+++ nfcRead.py
@@ -2,31 +2,50 @@ import os
 import RPi.GPIO as GPIO
 import datetime
 import csv
+import Adafruit_CharLCD as LCD
 
 from keypad import KeypadRead as KeypadRead
 
 #Dummy data
 flow = 5
 press = 1.3
-doctor = 1234
+#doctor = "1234"
 
-# print "Input doctor code"
-# digitOne = KeypadRead()
-# print digitOne
-# digitTwo = KeypadRead()
-# print digitTwo
-# digitThree = KeypadRead()
-# print digitThree
-# digitFour = KeypadRead()
-# print digitFour
+#initialization of the LCD
+lcd = LCD.Adafruit_CharLCDPlate()
 
-# doctor = digitOne + digitTwo + digitThree + digitFour
+#testing for LCD
+lcd.set_color(0,1,0)
+lcd.message("Input Doctor #")
+
+print "Input doctor code"
+digitOne = KeypadRead()
+lcd.clear()
+lcd.message("Input Doctor #\n" + digitOne)
+print digitOne
+digitTwo = KeypadRead()
+lcd.clear()
+lcd.message("Input Doctor #\n" + digitOne + digitTwo)
+print digitTwo
+digitThree = KeypadRead()
+lcd.clear()
+lcd.message("Input Doctor #\n" + digitOne + digitTwo + digitThree)
+print digitThree
+digitFour = KeypadRead()
+lcd.clear()
+lcd.message("Input Doctor #\n" + digitOne + digitTwo + digitThree + digitFour)
+print digitFour
+
+doctor = digitOne + digitTwo + digitThree + digitFour
 print doctor
 
 os.system('cd "/home/pi/libnfc/libnfc-libnfc-1.7.0/examples"')
-print "Scan NFC module"
-os.system('nfc-poll > /home/pi/RPiCode/nfc.txt') #Currently overwriting file every time. This file just used to save UID and then it is extracted and saved in the proper data file.
 
+print "Scan NFC module"
+lcd.clear()
+lcd.message("Scan NFC Module")
+
+os.system('nfc-poll > /home/pi/RPiCode/nfc.txt') #Currently overwriting file every time. This file just used to save UID and then it is extracted and saved in the proper data file.
 with open("nfc.txt") as f:
 	content = f.readlines()
 
@@ -35,6 +54,8 @@ for item in content:
 		code = item[item.index(':')+2:]
 ID = code.strip()
 UID = ID.replace(" ","")
+lcd.clear()
+lcd.message("Scan NFC Module\n" + UID)
 print UID
 
 path = os.listdir('/home/pi/RPiCode/')
