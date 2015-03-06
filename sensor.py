3dabeb6834113dfbb169ac7e2bd8b0ca745bd4b9
kms123 <sheasby23@me.com>
2015-03-06 16:34:58 -0600

    code to read from an analog sensor.

---
+++ sensor.py
@@ -0,0 +1,81 @@
+import time
+import os
+import RPi.GPIO as GPIO
+import Adafruit_CharLCD as LCD
+
+def readADC(adcnum, clockpin, mosipin, misopin, cspin):
+	GPIO.output(cspin, True)
+	GPIO.output(clockpin, False)
+	GPIO.output(cspin, False)
+	
+	commandout = adcnum
+	commandout |= 0x18
+	commandout <<= 3
+	
+	for i in range(5):
+		if(commandout & 0x80):
+			GPIO.output(mosipin, True)
+		else:
+			GPIO.output(mosipin, False)
+			
+		commandout <<= 1
+		GPIO.output(clockpin, True)
+		GPIO.output(clockpin, False)
+		
+	adcout = 0
+	
+	for i in range(12):
+		GPIO.output(clockpin, True)
+		GPIO.output(clockpin, False)
+		adcout <<= 1
+		if(GPIO.input(misopin)):
+			adcout |= 0x1
+		
+	GPIO.output(cspin, True)
+	
+	adcout >>= 1
+	return adcout
+	
+def sensor():
+	GPIO.setmode(GPIO.BCM)
+	
+	SPICLK = 11
+	SPIMISO = 9
+	SPIMOSI = 10
+	SPICS = 8
+	
+	# set up the SPI interface pins
+	GPIO.setup(SPIMOSI, GPIO.OUT)
+	GPIO.setup(SPIMISO, GPIO.IN)
+	GPIO.setup(SPICLK, GPIO.OUT)
+	GPIO.setup(SPICS, GPIO.OUT)
+	
+	sensorADC = 0
+	lastRead = 0
+	
+	sensing = True
+	data = []
+	
+	buttons = ((LCD.SELECT, True),
+				(LCD.LEFT, False),
+				(LCD.UP, True),
+				(LCD.DOWN, True),
+				(LCD.RIGHT, True))
+	
+	
+	while sensing:
+		
+		sensor = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+		
+		data.append(sensor)
+		
+		for button in buttons:
+			if lcd.is_pressed(button[0]):
+				sensing = button[1]
+				
+	
+	average = sum(data) / float(len(data))
+	
+	GPIO.cleanup()
+	
+	return average
